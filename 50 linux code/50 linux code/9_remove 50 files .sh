dc6ece31c42ac78e0248c7c073250d0ad9ce50b5
Abhishek-Bukshetwar <2022.abhishek.bukshetwar@ves.ac.in>
2025-04-22 08:06:39 +0530

    thanku

--- 50 linux code/50 linux code/9_remove 50 files .sh
+++ 50 linux code/50 linux code/9_remove 50 files .sh
@@ -1,4 +1,10 @@
-read -p "Enter file with similar name to delete " filename
-for i in {1..50}; 
-	do rm $filename"_"$i.sh; 
+read -p "Enter file base name to delete (e.g., script): " filename
+for i in {1..50}; do
+    file="${filename}_${i}.sh"
+    if [ -f "$file" ]; then
+        rm "$file"
+        echo "Deleted: $file"
+    else
+        echo "Not found: $file"
+    fi
 done
